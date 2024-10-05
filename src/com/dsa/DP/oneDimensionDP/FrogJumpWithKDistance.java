d8508d368e84f74af2eb03bd3c39d4cacc5bddb1
jeneesh01 <jeneesh.codeleap01@gmail.com>
2024-10-05 14:18:13 +0530

    add

---
+++ src/com/dsa/DP/oneDimensionDP/FrogJumpWithKDistance.java
@@ -0,0 +1,11 @@
+package com.dsa.DP.oneDimensionDP;
+
+
+// https://takeuforward.org/data-structure/dynamic-programming-frog-jump-with-k-distances-dp-4/
+
+
+public class FrogJumpWithKDistance {
+    public static void main(String[] args) {
+
+    }
+}
